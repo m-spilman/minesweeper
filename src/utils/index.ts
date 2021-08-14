f6f7d9e1683aa7a3d28aa44845f3654d2457a8e2
Matt Spilman <matthew.spilman@umbrage.com>
2021-08-14 12:33:12 -0500

    board with bombs and numbers

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,22 +1,79 @@
+import { MAX_COLS, MAX_ROWS, NUMBER_OF_BOMBS } from "../constants";
+// import { Cell, CellState, CellValue } from "../types";
+import { Cell, CellState} from "../types";
 
-import { MAX_COLS, MAX_ROWS } from "../constants";
-import {Cell, CellState, CellValue } from "../types";
+export const generateCells = (): Cell[][] => {
+  let cells: Cell[][] = [];
 
-
-export const generateCells = (): Cell[][] =>{
-
-    const cells:(Cell)[][] = [];
-    for( let row = 0; row<MAX_ROWS; row++){
-        cells.push([])
-        for (let col = 0; col<MAX_COLS; col++){
-            cells[row].push({
-                value: CellValue.none,
-                state: CellState.open
-              
-            })
-            
-
-        }
+  for (let row = 0; row < MAX_ROWS; row++) {
+    cells.push([]);
+    for (let col = 0; col < MAX_COLS; col++) {
+      cells[row].push({
+        // value: CellValue.none,
+        value: 0,
+        state: CellState.visible
+      });
     }
-    return cells
-}
+  }
+  let bombsPlaced = 0;
+  while (bombsPlaced < NUMBER_OF_BOMBS) {
+      console.log('correct bombs', NUMBER_OF_BOMBS)
+      console.log('placed', bombsPlaced)
+    const randomRow = Math.floor(Math.random() * MAX_ROWS);
+    const randomCol = Math.floor(Math.random() * MAX_COLS);
+
+    const currentCell = cells[randomRow][randomCol];
+    // if (currentCell.value !== CellValue.bomb) {
+        if (currentCell.value !== 9) {
+        console.log('in if')
+      cells[randomRow][randomCol] = {
+        ...cells[randomRow][randomCol],
+        // value: CellValue.bomb,
+        value: 9,
+       
+      };
+
+      bombsPlaced++;
+
+      const adjacentToBombArray = [
+        { row: randomRow - 1, column: randomCol },
+        { row: randomRow + 1, column: randomCol },
+        { row: randomRow, column: randomCol - 1 },
+        { row: randomRow, column: randomCol + 1 },
+        { row: randomRow - 1, column: randomCol + 1 },
+        { row: randomRow - 1, column: randomCol - 1 },
+        { row: randomRow + 1, column: randomCol + 1 },
+        { row: randomRow + 1, column: randomCol - 1 },
+      ];
+
+      for (let index = 0; index < adjacentToBombArray.length; index++) {
+        if (
+            adjacentToBombArray[index].row < MAX_ROWS &&
+            adjacentToBombArray[index].row !== -1 &&
+            adjacentToBombArray[index].column < MAX_COLS &&
+            adjacentToBombArray[index].column !== -1
+        ) {
+          if (
+            cells[adjacentToBombArray[index].row][
+                adjacentToBombArray[index].column
+            // ].value !== CellValue.bomb
+        ].value !== 9
+          ) {
+            cells[adjacentToBombArray[index].row][
+                adjacentToBombArray[index].column
+            ] = {
+              ...cells[adjacentToBombArray[index].row][
+                adjacentToBombArray[index].column
+              ],
+              value: cells[adjacentToBombArray[index].row][
+                adjacentToBombArray[index].column].value+1
+            };
+          }
+        }
+      }
+    }
+
+ 
+  }
+  return cells;
+};
